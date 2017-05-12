e6c18c54611418f62b7eab7be392cd84672ce226
joshua1b <joshua1b2823@gmail.com>
2017-05-12 23:01:13 +0900

    adding day3.go file. solved the problem day3

--- day3/day3.go
+++ day3/day3.go
@@ -4,39 +4,39 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"reflect"
 	"strconv"
 )
 
 type Integer int
 
 func (i Integer) isEven() bool {
-	half := i / 2.
-	typeOfHalf := reflect.TypeOf(half).String()
-	if typeOfHalf == "Integer" {
+	if i%2 == 0 {
 		return true
 	}
 	return false
+}
+
+func (i Integer) isWeird() string {
+	switch {
+	case !i.isEven():
+		return "Weird"
+
+	case i.isEven() && 2 <= i && i <= 5:
+		return "Not Weird"
+
+	case i.isEven() && 6 <= i && i <= 20:
+		return "Weird"
+
+	case i.isEven() && 20 < i:
+		return "Not Weird"
+	}
+	return ""
 }
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	str, _ := reader.ReadString('\n')
 	n, _ := strconv.Atoi(str)
-
 	num := Integer(n)
-
-	switch {
-	case !num.isEven():
-		fmt.Println("Weird")
-
-	case num.isEven() && 2 <= num && num <= 5:
-		fmt.Println("Not Weird")
-
-	case num.isEven() && 6 <= num && num <= 20:
-		fmt.Println("Weird")
-
-	case num.isEven() && 20 < num:
-		fmt.Println("Not Weird")
-	}
+	fmt.Println(num.isWeird())
 }
